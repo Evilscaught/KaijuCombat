7fb9f26d0a51efd2fc2d9d5440d09863987a1344
Evilscaught <evilscaught@gmail.com>
2019-11-21 11:51:07 -0700

    Added scene 'EscapeUI': +Contains the quick escape menu that will later be implemented to the game scene +Added script 'EscapeUIController.cs' to 'Assets/Scripts/'

---
+++ KaijuCombat/Assets/Scripts/EscapeUIController.cs
@@ -0,0 +1,47 @@
+﻿/* Author(s):           Scott McKay
+ * Group:               The Omnipotent Creators
+ * Date of Creation:    Thursday, November 14th, 2019 @11:00 p.m. M.S.T. (Autumn Semester MMXIX)
+ * Class:               Game Engines
+ * Instructor:          Michael Cassens
+ * Institution:         The University of Montana
+ * 
+ * Purpose of Class:    Implements escape functionality during the game session for the user.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeUIController : MonoBehaviour
+{
+    public GameObject escapeUI;
+    private bool isShowing;
+
+    // Update is called once per frame
+    void Update()
+    { 
+        //If the user presses the 'escape' key, make quick menu visible.
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isShowing = !isShowing;
+            escapeUI.SetActive(isShowing);
+        }
+    }
+
+    /// <summary>
+    /// Hides the quick menu
+    /// </summary>
+    public void Resume()
+    {
+        isShowing = !isShowing;
+        escapeUI.SetActive(isShowing);
+    }
+
+    /// <summary>
+    /// Exits the Unity Game
+    /// </summary>
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
